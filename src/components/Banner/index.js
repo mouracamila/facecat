7cd5085a6387021be89590a320b87de6b38c6e87
Camila Moura <camilapaleo@gmail.com>
2020-06-01 22:37:24 +0200

    created Banner Component

---
+++ src/components/Banner/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Container } from "./styles";
+
+function Banner() {
+  return (
+    <Container>
+      <h1>FaceCat Social Network </h1>
+    </Container>
+  );
+}
+
+export default Banner;
---
+++ src/components/Banner/styles.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  height: 380px;
+  width: 100%;
+  background: cornflowerblue;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  color: #fff;
+`;
