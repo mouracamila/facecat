cddcac6d4c493e00fe4807dd851d0892dff3681b
Camila Moura <camilapaleo@gmail.com>
2020-06-02 15:38:51 +0200

    work in progress in Dashboard styles

---
+++ src/pages/Dashboard/styles.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  div.wrapper-content {
+    display: flex;
+  }
+
+  div.wrapper-content .timeline {
+    flex: 1;
+    margin: 10px 10px 0;
+  }
+`;
